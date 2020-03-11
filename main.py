b380defb65483cd7c41543c77ecdebf20f7ce1bd
ALex Park <alexpark@system76-pc.localdomain>
2020-03-11 01:39:35 -0400

    time calculation added

--- main.py
+++ main.py
@@ -31,19 +31,36 @@ def collect(run_num, ssid_name):
     rssi_container_netw = []
     rssi_container_beaconf = []
 
+    time_container_acp = []
+    time_container_netw = []
+    time_container_beaconf = []
+
     sec = 0
     try:
         while True:
             print('>> Timestamp: ' + str(sec))
             sec += 1
+            
+            start_time = time.time()
             rssi_val_from_acp = rssi_with_access_point(ssid_name) #from access point
+            time_acp = time.time() - start_time
+
+            start_time = time.time()
             rssi_val_from_netw = rssi_with_netwmanager(ssid_name) #from command (nmcli - network manager)
+            time_netw = time.time() - start_time
+
+            start_time = time.time()
             rssi_val_from_beaconf = rssi_with_beaconf(ssid_name, sudo_pw) #from probing beacon frame
+            time_beaconf = time.time() - start_time
 
             rssi_container_acp.append(rssi_val_from_acp)
             rssi_container_netw.append(rssi_val_from_netw)
             rssi_container_beaconf.append(rssi_val_from_beaconf)
             
+            time_container_acp.append(time_acp)
+            time_container_netw.append(time_netw)
+            time_container_beaconf.append(time_beaconf)
+
             print('> Access P: ', rssi_container_acp)
             print('> Network M: ', rssi_container_netw)
             print('> Beacon F: ', rssi_container_beaconf)
@@ -53,10 +70,16 @@ def collect(run_num, ssid_name):
     except KeyboardInterrupt:
         print('Exit the Collection process!')
 
+        #data save
         total_container = {}
         total_container['rssi_acp'] = rssi_container_acp
         total_container['rssi_netw'] = rssi_container_netw
         total_container['rssi_beaconf'] = rssi_container_beaconf
+
+        #time save
+        total_container['time_acp'] = time_container_acp
+        total_container['time_netw'] = time_container_netw
+        total_container['time_beaconf'] = time_container_beaconf
 
         f = open(venue + '_rssi_val_' + str(run_num) + '.pkl', 'wb')
         pickle.dump(total_container, f)
